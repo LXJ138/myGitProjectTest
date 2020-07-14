3d9674cf24f26df96c83ef8d427cfbff068f3188
linxiaojun <2077194933@qq.com>
2020-07-14 18:16:14 +0800

    study09

---
+++ videoviewdemo/src/main/java/com/example/videoviewdemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.videoviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private VideoView videoView;
+    private Button btn_start;
+    private Button btn_pause;
+    private Button btn_stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+    }
+
+    private void bindViews() {
+        videoView = (VideoView) findViewById(R.id.videoView);
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_pause = (Button) findViewById(R.id.btn_pause);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+
+        btn_start.setOnClickListener(this);
+        btn_pause.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+
+        //根据文件路径播放
+        /*if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/lesson.mp4");
+        }*/
+
+        //读取放在raw目录下的文件
+        videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/" + R.raw.lesson));
+        videoView.setMediaController(new MediaController(this));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                videoView.start();
+                break;
+            case R.id.btn_pause:
+                videoView.pause();
+                break;
+            case R.id.btn_stop:
+                videoView.stopPlayback();
+                break;
+        }
+    }
+}
